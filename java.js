6cefee3036df59cac09b17a93651ac055edb2861
joyasaha1004 <joyasaha440@gmail.com>
2024-02-27 08:09:08 -0600

    project

---
+++ java.js
@@ -0,0 +1,6 @@
+const log_in = document.querySelector(".log_in")
+const for_join = log_in.querySelector(".for_join");
+const btn2 = document.querySelector(".btn2")
+btn2.onclick = ()=>{
+    for_join.classList.add("activeLog");
+}
